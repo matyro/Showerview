54e801fa5baf4877e5a84e4f9881da292bde90c4
Dominik B <dominik.baack@udo.edu>
2015-11-12 15:21:20 +0100

    Moved zmq files
    -> own socket implementation should be better

--- src/Network/Socket.h
+++ src/Network/Socket.h
@@ -14,21 +14,19 @@ public:
 	virtual ~Socket();
 
 	// Server initialization
-	virtual bool create();
-	virtual bool bind(const int port);
-	virtual bool listen() const;
-	virtual bool accept(Socket&) const;
+	bool create();
+	bool bind(const int port);
+	bool listen() const;
+	bool accept(Socket&) const;
 
 	// Client initialization
-	virtual bool connect(const std::string host, const int port);
+	bool connect(const std::string host, const int port);
 
 	// Data Transimission
-	virtual bool send(char*, unsigned int) const;
-	virtual std::vector<char>&& recv() const;
+	bool send(char*, unsigned int) const;
+	std::vector<char>&& recv() const;
 
-	virtual void set_non_blocking(const bool);
-
-	virtual bool is_valid() const;
+	bool is_valid() const;
 
 
 };
